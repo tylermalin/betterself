4d214186e352d5d01d0ccc00178eece8149ee5bf
tyler malin <tylermalin@gmail.com>
2019-06-13 03:34:10 -0700

    Update local.py
    t

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,6 +6,18 @@ Local settings
 - Use console backend for emails
 - Add Django Debug Toolbar
 """
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 from config.settings.constants import LOCAL
 
 print('Using {} configurations'.format(__name__))
